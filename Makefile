e6c199473ed271cee921af6597e30404fb7c2eb7
Islandparrott <93695101+Islandparrott@users.noreply.github.com>
2023-02-20 13:17:15 -0800

    Add files via upload
    These files contain the code used to spawn entities on the sides of screens, give them random movement and slight curves.

--- Makefile
+++ Makefile
@@ -1,33 +1,9 @@
-# add object files here and make a rule down below
-OBJECTS = cosmotost.o Box.o Global.o libggfonts.a Menu.o
-
-FLAGS =  -Wall -lX11 -lGL -lGLU -lm
-COMP = g++ -std=c++17 
-
 
 all: cosmotost
 
-cosmotost: cosmotost.o Box.o Global.o Menu.o
-	$(COMP) -o cosmotost $(OBJECTS) $(FLAGS)
-
-cosmotost.o: cosmotost.cpp
-	$(COMP) -c cosmotost.cpp
-
-Box.o: Box.cpp Box.h
-	$(COMP) -c Box.cpp
-
-Global.o: Global.cpp Global.h
-	$(COMP) -c Global.cpp
-
-# still in development
-Menu.o: Menu.cpp Menu.h
-	$(COMP) -c Menu.cpp
+cosmotost: cosmotost.cpp
+	g++ cosmotost.cpp -Wall -ocosmotost -lX11 -lGL -lGLU -lm
 
 clean:
-	rm -f cosmotost *.o
+	rm -f cosmotost
 
-# calling make run ensures cerr is redirected to a log file
-# for the purposes of debugging
-
-run:
-	./cosmotost 2> err.log
--- cosmotost.cpp
+++ cosmotost.cpp
@@ -1,14 +1,8 @@
-/********************************************************************
-*
-*		Program: cosmotost
-*		Developed by: DAHM
-*   	Group 1: (D)aniel, (A)iland, (H)uaiyu, (M)ichael
-*		Date Code Originated: 2/17/23
-*		Base Code Provided by: Professor Gordon Griesel
-*
-**********************************************************************/
-
-//                 		INCLUDES
+/* Ailand Parriott
+ * created: 23.02.17
+ * 3350
+ * Cosmotost entity maker
+ */
 
 #include <iostream>
 #include <stdio.h>
@@ -21,24 +15,62 @@
 #include <X11/keysym.h>
 #include <GL/glx.h>
 
-
-// 					LOCAL LIBRARIES
-#include "fonts.h"	// used for fonts
-#include "Box.h"	// base box class
-#include "Global.h"	// global vars
-#include "Menu.h"	// menu
-
 using namespace std;
 
-// Global g;
-// Box b1;
-// extern Box b1;
+class Global {
+    public:
+	int xres, yres;
+	int mousex, mousey;
+    int numEnt;
+
+    Global() {
+        xres = 1280;
+        yres = 720;
+    }
+} g;
+
+const int MAX_ENTITIES = 128;
+
+class Entity {
+	public:
+	float dim[2];
+	float vel[2];
+	float pos[2];
+    float curve[2];
+	unsigned char color[3];
+	/*void set_color(unsigned char col[3]) {
+		memcpy(color,  col, sizeof(unsigned char) * 3);
+	}*/
+
+	Entity() {
+		dim[0] = 4;
+		dim[1] = 4;
+		pos[0] = g.xres / 2;
+		pos[1] = g.yres / 2;
+		vel[0] = -1;
+		vel[1] = -1;
+        curve[0] = 0;
+        curve[1] = 0;
+	}
+
+	Entity(float wid, float hgt, float posX, float posY, float v0, float v1, float curveX, float curveY) {
+		dim[0] = wid;
+		dim[1] = hgt;
+		pos[0] = posX;
+		pos[1] = posY;
+		vel[0] = v0;
+		vel[1] = v1;
+        curve[0] = curveX;
+        curve[1] = curveY;
+	}
+} entity[MAX_ENTITIES];
 
 class X11_wrapper {
 private:
 	Display *dpy;
 	Window win;
 	GLXContext glc;
+
 public:
 	~X11_wrapper();
 	X11_wrapper();
@@ -60,9 +92,9 @@ void render(void);
 //=====================================
 // MAIN FUNCTION IS HERE
 //=====================================
-int main()
-{
+int main() {
 	init_opengl();
+
 	//Main loop
 	int done = 0;
 	while (!done) {
@@ -78,242 +110,265 @@ int main()
 		x11.swapBuffers();
 		usleep(200);
 	}
+
 	return 0;
 }
 
-X11_wrapper::~X11_wrapper()
-{
-	XDestroyWindow(dpy, win);
-	XCloseDisplay(dpy);
+#define rnd() ((float)rand() / (float)RAND_MAX)
+int randnum(int min, int max) {
+    return min + rand() % ((max + 1) - min);
 }
 
-X11_wrapper::X11_wrapper()
-{
-	GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-	int w = g.xres, h = g.yres;
-	dpy = XOpenDisplay(NULL);
-	if (dpy == NULL) {
-		cout << "\n\tcannot connect to X server\n" << endl;
-		exit(EXIT_FAILURE);
+void makeEntity(float posX, float posY, float initVelX, float initVelY, float curveX, float curveY) {
+	if (g.numEnt < MAX_ENTITIES) {
+		entity[g.numEnt].dim[0] = 4;
+		entity[g.numEnt].dim[1] = 4;
+		entity[g.numEnt].pos[0] = posX;
+		entity[g.numEnt].pos[1] = posY;
+		entity[g.numEnt].vel[0] = initVelX;
+		entity[g.numEnt].vel[1] = initVelY;
+		entity[g.numEnt].color[0] = randnum(80, 120);
+		entity[g.numEnt].color[1] = randnum(100, 180);
+		entity[g.numEnt].color[2] = randnum(200, 255);
+        entity[g.numEnt].curve[0] = curveX;
+        entity[g.numEnt].curve[1] = curveY;
+		g.numEnt++;
+	}	
+}
+
+X11_wrapper::~X11_wrapper() {
+    XDestroyWindow(dpy, win);
+    XCloseDisplay(dpy);
+}
+
+X11_wrapper::X11_wrapper() {
+    GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+    int w = g.xres, h = g.yres;
+    dpy = XOpenDisplay(NULL);
+    if (dpy == NULL) {
+        cout << "\n\tcannot connect to X server\n" << endl;
+        exit(EXIT_FAILURE);
+    }
+    Window root = DefaultRootWindow(dpy);
+    XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
+    if (vi == NULL) {
+        cout << "\n\tno appropriate visual found\n" << endl;
+        exit(EXIT_FAILURE);
+    } 
+    Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+    XSetWindowAttributes swa;
+    swa.colormap = cmap;
+    swa.event_mask =
+        ExposureMask | KeyPressMask | KeyReleaseMask |
+        ButtonPress | ButtonReleaseMask |
+        PointerMotionMask |
+        StructureNotifyMask | SubstructureNotifyMask;
+    win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
+        InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+    set_title();
+    glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+    glXMakeCurrent(dpy, win, glc);
+}
+
+void X11_wrapper::set_title() {
+    //Set the window title bar.
+    XMapWindow(dpy, win);
+    XStoreName(dpy, win, "Cosmotost");
+}
+
+bool X11_wrapper::getXPending() {
+    //See if there are pending events.
+    return XPending(dpy);
+}
+
+XEvent X11_wrapper::getXNextEvent() {
+    //Get a pending event.
+    XEvent e;
+    XNextEvent(dpy, &e);
+    return e;
+}
+
+void X11_wrapper::swapBuffers() {
+    glXSwapBuffers(dpy, win);
+}
+
+void X11_wrapper::reshape_window(int width, int height) {
+    //window has been resized.
+    g.xres = width;
+    g.yres = height;
+    //
+    glViewport(0, 0, (GLint)width, (GLint)height);
+    glMatrixMode(GL_PROJECTION); glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+    glOrtho(0, g.xres, 0, g.yres, -1, 1);
+}
+
+void X11_wrapper::check_resize(XEvent *e) {
+    //The ConfigureNotify is sent by the
+    //server if the window is resized.
+    if (e->type != ConfigureNotify)
+        return;
+    XConfigureEvent xce = e->xconfigure;
+    if (xce.width != g.xres || xce.height != g.yres) {
+        //Window size did change.
+        reshape_window(xce.width, xce.height);
+    }
+}
+//-------------------------------------------------------------------------
+
+void X11_wrapper::check_mouse(XEvent *e) {
+    static int savex = 0;
+    static int savey = 0;
+    //Weed out non-mouse events
+    if (e->type != ButtonRelease &&
+            e->type != ButtonPress &&
+            e->type != MotionNotify) {
+        //This is not a mouse event that we care about.
+        return;
+    }
+    //
+    if (e->type == ButtonRelease) {
+        return;
+    }
+    if (e->type == ButtonPress) {
+        if (e->xbutton.button==1) {
+            //Left button was pressed.
+            //int y = g.yres - e->xbutton.y;
+            return;
+        }
+        if (e->xbutton.button==3) {
+            //Right button was pressed.
+            //makeEntity(e->xbutton.x, g.yres - e->xbutton.y);
+            return;
+        }
+    }
+    if (e->type == MotionNotify) {
+        //The mouse moved!
+        if (savex != e->xbutton.x || savey != e->xbutton.y) {
+            /*savex = e->xbutton.x;
+            savey = e->xbutton.y;
+            //Code placed here will execute whenever the mouse moves.
+            makeEntity(e->xbutton.x, g.yres - e->xbutton.y);
+            */
+        }
+    }
+}
+
+int X11_wrapper::check_keys(XEvent *e) {
+    if (e->type != KeyPress && e->type != KeyRelease) {
+        return 0;
+    }
+    int key = XLookupKeysym(&e->xkey, 0);
+    if (e->type == KeyPress) {
+        switch (key) {
+            case XK_1:
+                //Key 1 was pressed
+                break;
+            case XK_Escape:
+                //Escape key was pressed
+                return 1;
+        }
+    }
+    return 0;
+}
+
+void init_opengl(void) {
+    //OpenGL initialization
+    glViewport(0, 0, g.xres, g.yres);
+    //Initialize matrices
+    glMatrixMode(GL_PROJECTION); 
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); 
+    glLoadIdentity();
+    //Set 2D mode (no perspective)
+    glOrtho(0, g.xres, 0, g.yres, -1, 1);
+    //Set the screen background box.color
+    glClearColor(0.1, 0.1, 0.1, 1.0);
+    // TEXT ENABLE
+    glEnable(GL_TEXTURE_2D);
+}
+
+int entityEnter, chainNum = 0, everyOther = 0;
+float spawnX, spawnY, spawnVelX, spawnVelY, curveRandX = 0, curveRandY = 0;
+
+void physics() {
+    // makeEntity(int posX, int posY,  int initVelX, int initVelY) 
+
+    // could make it gen a random number, check if it is greater than 0, then count down each time??
+    if (everyOther == 0) {
+        everyOther = 4;
+        if (chainNum == 0) {
+            chainNum = randnum(4, 12);
+            curveRandX = randnum(-4, 0);
+            curveRandY = randnum(-4, 4);
+
+            entityEnter = randnum(0, 3);
+            if (entityEnter == 0) {
+                // makeEntity SPAWN FROM TOP, MOVES LEFT DOWNWARD
+                spawnX = randnum(g.xres / 2, g.xres);
+                spawnY = g.yres - 5;
+                spawnVelX = randnum(-8, -4);
+                spawnVelY = randnum(-8, 0);
+            } else if (entityEnter <= 2) {
+                // makeEntity SPAWN FROM RIGHT, MOVES LEFT, RANDUM UP AND DOWN
+                spawnX = g.xres;
+                spawnY = randnum(0, g.yres);
+                spawnVelX = randnum(-8, -4);
+                spawnVelY = randnum(-8, 8);
+            } else if (entityEnter == 3) {
+                // makeEntity SPAWN FROM BOTTOM, MOVES LEFT AND UP
+                spawnX = randnum(g.xres / 2, g.xres);
+                spawnY = 5;
+                spawnVelX = randnum(-8, -4);
+                spawnVelY = randnum(0, 8);
+            }
+        }
+        makeEntity(spawnX, spawnY, spawnVelX, spawnVelY, curveRandX, curveRandY);
+
+        chainNum--;
+    }
+    everyOther--;
+
+	for (int i = 0; i < g.numEnt; i++) {
+        entity[i].pos[0] += entity[i].vel[0];
+		entity[i].pos[1] += entity[i].vel[1];
+
+        entity[i].vel[0] += entity[i].curve[0] / 16;
+        entity[i].vel[1] += entity[i].curve[1] / 16;
+
+        // DESPAWN
+        if (entity[i].pos[1] < -4 || 
+                entity[i].pos[1] > g.yres + 4 ||
+                entity[i].pos[0] < -4) {	
+            entity[i] = entity[--g.numEnt];
+        }
+
+        // BOUNCE
+		if (entity[i].pos[1] <= 4 ||
+				entity[i].pos[1] >= g.yres - 4) {			
+			entity[i].vel[1] = -entity[i].vel[1];
+        }
+    }
+
+}
+
+void render() {
+
+	glClear(GL_COLOR_BUFFER_BIT);   
+
+	//Draw entoty.
+	for (int i = 0; i < g.numEnt; i++) {
+		glPushMatrix();
+		glColor3ubv(entity[i].color);
+		glTranslatef(entity[i].pos[0], entity[i].pos[1], 0.0f);
+		glBegin(GL_QUADS);
+			glVertex2f(-entity[i].dim[0], -entity[i].dim[1]);
+			glVertex2f(-entity[i].dim[0],  entity[i].dim[1]);
+			glVertex2f( entity[i].dim[0],  entity[i].dim[1]);
+			glVertex2f( entity[i].dim[0], -entity[i].dim[1]);
+		glEnd();
+		glPopMatrix();
 	}
-	Window root = DefaultRootWindow(dpy);
-	XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
-	if (vi == NULL) {
-		cout << "\n\tno appropriate visual found\n" << endl;
-		exit(EXIT_FAILURE);
-	} 
-	Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
-	XSetWindowAttributes swa;
-	swa.colormap = cmap;
-	swa.event_mask =
-		ExposureMask | KeyPressMask | KeyReleaseMask |
-		ButtonPress | ButtonReleaseMask |
-		PointerMotionMask |
-		StructureNotifyMask | SubstructureNotifyMask;
-	win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
-		InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
-	set_title();
-	glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
-	glXMakeCurrent(dpy, win, glc);
-
-	// set initial game state
-	g.state = MAINMENU;
-}
-
-void X11_wrapper::set_title()
-{
-	//Set the window title bar.
-	XMapWindow(dpy, win);
-	XStoreName(dpy, win, "c0sm0t0asT");
-}
-
-bool X11_wrapper::getXPending()
-{
-	//See if there are pending events.
-	return XPending(dpy);
-}
-
-XEvent X11_wrapper::getXNextEvent()
-{
-	//Get a pending event.
-	XEvent e;
-	XNextEvent(dpy, &e);
-	return e;
-}
-
-void X11_wrapper::swapBuffers()
-{
-	glXSwapBuffers(dpy, win);
-}
-
-void X11_wrapper::reshape_window(int width, int height)
-{
-	//window has been resized.
-	g.xres = width;
-	g.yres = height;
-	//
-	glViewport(0, 0, (GLint)width, (GLint)height);
-	glMatrixMode(GL_PROJECTION); glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-	glOrtho(0, g.xres, 0, g.yres, -1, 1);
-}
-
-void X11_wrapper::check_resize(XEvent *e)
-{
-	//The ConfigureNotify is sent by the
-	//server if the window is resized.
-	if (e->type != ConfigureNotify)
-		return;
-	XConfigureEvent xce = e->xconfigure;
-	if (xce.width != g.xres || xce.height != g.yres) {
-		//Window size did change.
-
-		reshape_window(xce.width, xce.height);
-		// reshape_window(g.xres, g.yres);	// force game to be 600x800 - test
-
-	}
-}
-//-----------------------------------------------------------------------------
-
-void X11_wrapper::check_mouse(XEvent *e)
-{
-	static int savex = 0;
-	static int savey = 0;
-
-	//Weed out non-mouse events
-	if (e->type != ButtonRelease &&
-		e->type != ButtonPress &&
-		e->type != MotionNotify) {
-		//This is not a mouse event that we care about.
-		return;
-	}
-	//
-	if (e->type == ButtonRelease) {
-		return;
-	}
-	if (e->type == ButtonPress) {
-		if (e->xbutton.button==1) {
-			//Left button was pressed.
-			//int y = g.yres - e->xbutton.y;
-			return;
-		}
-		if (e->xbutton.button==3) {
-			//Right button was pressed.
-			return;
-		}
-	}
-	if (e->type == MotionNotify) {
-		//The mouse moved!
-		if (savex != e->xbutton.x || savey != e->xbutton.y) {
-			savex = e->xbutton.x;
-			savey = e->xbutton.y;
-			//Code placed here will execute whenever the mouse moves.
-
-
-		}
-	}
-}
-
-int X11_wrapper::check_keys(XEvent *e)
-{
-	if (e->type != KeyPress && e->type != KeyRelease)
-		return 0;
-	int key = XLookupKeysym(&e->xkey, 0);
-	if (e->type == KeyPress) {
-		switch (key) {
-			case XK_1:
-				//Key 1 was pressed
-				break;
-			case XK_Escape:
-				//Escape key was pressed
-				return 1;
-		}
-	}
-	return 0;
-}
-
-// won't have to mess with this much in the project
-void init_opengl(void)
-{
-	//OpenGL initialization
-    // make the viewport the size of the whole window
-	glViewport(0, 0, g.xres, g.yres);
-	//Initialize matrices
-    // most likely this will always be set this way in all our programs
-    // vertices are drawn and then filled in
-	glMatrixMode(GL_PROJECTION); glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-	//Set 2D mode (no perspective)
-    // modifies the matrices to be in orthographic mode
-    // think of it as a camera looking at things from so far away
-    //     that it is in 2 dimensions
-	glOrtho(0, g.xres, 0, g.yres, -1, 1);
-	//Set the screen background color
-    // makes it almost black
-	
-
-
-    // initialize fonts
-    
-	glEnable(GL_TEXTURE_2D);
-	initialize_fonts();
-
-}
-
-void physics()
-{
-
-}
-
-void render()
-{
-	
-
-	if (g.state == MAINMENU) {
-		glClearColor(24.0/255, 38.0/255, 37.0/255, 1.0);	// clear it to a bluish
-		glClear(GL_COLOR_BUFFER_BIT);	// clear screen
-		
-		// // make text boxes
-
-
-		// // make it so that the box is only visible if the screen is big enough
-		
-		// s_menu_bg.set_color(2, 115, 104);
-		// glColor3ubv(s_menu_bg.color);
-        
-		// glPushMatrix();
-		// glTranslatef(g.xres/2.0f, g.yres/2.0f, 0.0f);
-		// glBegin(GL_QUADS);
-		// 	glVertex2f(-s_menu_bg.w, -s_menu_bg.w);
-		// 	glVertex2f(-s_menu_bg.w,  s_menu_bg.w);
-		// 	glVertex2f( s_menu_bg.w,  s_menu_bg.w);
-		// 	glVertex2f( s_menu_bg.w, -s_menu_bg.w);
-		// glEnd();
-		// glPopMatrix();
-
-		// Rect r;
-		// r.bot = g.yres/2.0f;
-		// r.left = g.xres/2.0f;
-		// r.center = 1;
-
-		// ggprint8b(&r, 16, 0x00011F26, "c0sm0t0asT");
-
-		mm.draw();
-
-		
-	} else if (g.state == GAME) {
-
-
-
-	} else if (g.state == PAUSE) {
-
-
-	} else if (g.state == GAMEOVER) {
-
-
-	}
-
-	
 
 }
 
